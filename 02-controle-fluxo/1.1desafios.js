3a9f961f191cfd8eebab388c398839f15b118064
JulianoLopesM <monteirojjuliano@gmail.com>
2025-11-12 12:31:43 -0300

    desafio ifs

--- 02-controle-fluxo/1.1desafios.js
+++ 02-controle-fluxo/1.1desafios.js
@@ -20,3 +20,14 @@ if(nota >=9.5)  {
     console.log(nota = "F");
 }
 console.log("Nota convertida:", nota);
+
+const ano = 2024;
+// Um ano é bissexto se:
+// - É divisível por 4 E NÃO é divisível por 100
+// OU
+// - É divisível por 400
+if((ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0)){
+    console.log(ano + " É um ano bissexto")
+} else {
+    console.log(ano + " Não é um ano bissexto")
+}
