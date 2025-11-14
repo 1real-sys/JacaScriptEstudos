3678b8f46349909476e3933db87e99cf2a017341
JulianoLopesM <monteirojjuliano@gmail.com>
2025-11-14 12:47:25 -0300

    desafios 2 finalizado controle de fluxo

--- 02-controle-fluxo/2.1desafio.js
+++ 02-controle-fluxo/2.1desafio.js
@@ -9,7 +9,7 @@ for(var i = 0; i <=10 ; i++){
 
 
 
-for(numero = 2; numero <= 50; numero++){
+for(var numero = 2; numero <= 50; numero++){
     let ehPrimo = true;
     for( let divisor = 2; divisor < numero; divisor++ ){
         if(numero % divisor === 0){
@@ -20,5 +20,31 @@ for(numero = 2; numero <= 50; numero++){
 
     if(ehPrimo){
         console.log(numero + " é primo")
+    }
+}
+
+let numero1 = 5
+let fatorial = 1
+let contador = numero1;
+
+while(contador > 1){
+    fatorial *= contador;
+    contador--;
+
+}
+
+console.log(`Fatorial de ${numero1} = ${fatorial}`);
+
+
+
+for(let t = 1; t <= 100; t++){
+    if(t % 5 ===0 && t % 3 === 0){
+        console.log(t + " FizzBuzz");
+    } else if (t % 5 === 0){
+        console.log(t + " Buzz");
+    } else if (t % 3 === 0){
+        console.log(t + " Fizz");
+    }else{
+        console.log( t + " Not a fizz or buzz");
     }
 }
